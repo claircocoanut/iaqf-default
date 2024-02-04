aa5bf81f99da5a5812c5e7bd03ac286a56c9a788
Clair <claircui@uchicago.edu>
2024-02-04 01:00:21 -0600

    add explore data

--- data_analysis/explore_data.py
+++ data_analysis/explore_data.py
@@ -1,25 +1,14 @@
 import pandas as pd
 import numpy as np
-from statsmodels.tsa.stattools import adfuller
 from matplotlib import colors
 import matplotlib.pyplot as plt
+from typing import Callable, Any
+
+import statsmodels.api as sm
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 
-def hurst_exponent(x, max_lag=20):
-    """Returns the Hurst Exponent of the time series"""
-
-    lags = range(2, max_lag)
-
-    # variances of the lagged differences
-    tau = [x.diff(lag).std() for lag in lags]
-
-    # calculate the slope of the log plot -> the Hurst Exponent
-    reg = np.polyfit(np.log(lags), np.log(tau), 1)
-
-    return reg[0]
-
-
-def calc_rolling_stat(df: pd.DataFrame, month: int = 3) -> pd.DataFrame:
+def calc_rolling_stat(df: pd.DataFrame, month: int = 6) -> pd.DataFrame:
     """
     Calculate rolling period aggregate statistics
 
@@ -53,41 +42,262 @@ def calc_rolling_stat(df: pd.DataFrame, month: int = 3) -> pd.DataFrame:
     n = month * 20 + 1
     ret = df.rolling(f"{n}D").aggregate([ret, vol, ret_max, ret_min]).shift(-n)
 
-    return ret.T.swaplevel().T
+    return ret.stack(level=0).swaplevel()
 
 
-def plot_change_scatter(s: pd.Series, df_mpd: pd.DataFrame):
+def rename_stat_df(df_stat: pd.DataFrame,
+                   name_map: dict[str, str]) -> pd.DataFrame:
+    """
+    Rename stat DataFrame [ticker] -> probability dataframe [market]
+
+    Parameters
+    ----------
+    df_stat: pd.DataFrame
+        Index: (ticker, date)
+        Columns: ret, vol, ret_min, ret_max
+    name_map: dict
+        dictionary of {ticker: market}, e.g. {"sp6m": "SPY", "sp12m": "SPY"}
+
+    Returns
+    -------
+    pd.DataFrame:
+        Index: (market, date)
+        Columns: ret, vol, ret_min, ret_max
+    """
+
+    df_list = []
+    keys = []
+    for k, v in name_map.items():
+        df_list.append(df_stat.loc[v])
+        keys.append(k)
+
+    return pd.concat(df_list, axis=0, keys=keys).unstack(level=0)
+
+
+def merge_prob_stat(df_stat: pd.DataFrame,
+                    df_mpd: pd.DataFrame) -> pd.DataFrame:
+    """
+    Combine probability and return data
+
+    Parameters
+    ----------
+    df_stat: pd.DataFrame of rolling stat calculated
+    df_mpd: pd.DataFrame of bi-weekly MPD data
+
+    Returns
+    -------
+    pd.DataFrame:
+        Columns:
+            prDec: probability of large decrease
+            prInc: probability of large increase
+            ret: next X-month return
+            vol: next X-month close-to-close volatility
+            ret_min: next X-month maximum drawdown
+            ret_max: next X_month maximum return
+    """
+    comp = pd.concat([
+        df_mpd.set_index(["market", "idt"])[["prDec", "prInc"]],
+        df_stat.stack().swaplevel()], axis=1).sort_index()
+    comp[df_stat.stack().columns].ffill(inplace=True)
+    comp = comp.dropna(how="any")
+    comp.index.names = ("market", "idt")
+
+    return comp
+
+
+def hurst_exponent(x, max_lag=20):
+    """Returns the Hurst Exponent of the time series"""
+
+    lags = range(2, max_lag)
+
+    # variances of the lagged differences
+    tau = [x.diff(lag).std() for lag in lags]
+
+    # calculate the slope of the log plot -> the Hurst Exponent
+    reg = np.polyfit(np.log(lags), np.log(tau), 1)
+
+    return reg[0]
+
+
+def plot_change_scatter(df: pd.DataFrame, col: str,
+                        hline_levels: list[float] = None):
     """
     Scatter plot for return / vol -> decrease / increase probability
 
     Parameters
     ----------
-    s:  return / vol series to investigate
+    df: pd.DataFrame
+        Columns: at least ["prDec", "prInc", col]
         Index: (market: str, date: DatetimeIndex)
-    df_mpd: pd.DataFrame of market-based probability
-        Columns: at least ["prDec", "prInc"]
-        Index: (market: str, date: DatetimeIndex)
+    col: str
+        Column name of prDec / prInc -> series plot
     """
 
-    comp = pd.concat([
-        df_mpd.set_index(["market", "idt"])[["prDec", "prInc"]],
-        s], axis=1).sort_index().ffill()
+    tickers = df.index.get_level_values("market").unique()
+    color_map = dict(zip(
+        tickers, list(colors.TABLEAU_COLORS.keys())[:len(tickers)]))
 
-    tickers = comp.index.get_level_values("market")
-    color_map = dict(zip(colors.TABLEAU_COLORS[:len(tickers)], tickers))
+    fig, ax = plt.subplots(1, 2, figsize=(12, 4))
 
-    fig, ax = plt.subplots(2, 1, figsize=(12, 4))
-
-    ax[0].axhline(y=0.2, c="red")
-    ax[0].axhline(y=-0.2, c="red")
+    # Scatter Plot
     [g.plot.scatter(
-        x="prDec", y=s.name, ax=ax[0], s=10, label=name,
+        x="prDec", y=col, ax=ax[0], s=10, label=name,
         color=color_map[name], alpha=0.5
-    ) for name, g in comp.groupby("ticker")]
+    ) for name, g in df.groupby("market")]
 
-    ax[1].axhline(y=0.2, c="red")
-    ax[1].axhline(y=-0.2, c="red")
     [g.plot.scatter(
-        x="prInc", y=s.name, ax=ax[1], s=10, label=name,
+        x="prInc", y=col, ax=ax[1], s=10, label=name,
         color=color_map[name], alpha=0.5
-    ) for name, g in comp.groupby("ticker")]
+    ) for name, g in df.groupby("market")]
+
+    if hline_levels is not None:
+        for i in hline_levels:
+            ax[0].axhline(y=i, c="red")
+            ax[1].axhline(y=i, c="red")
+
+    plt.show()
+
+
+def prep_regression_stat(df: pd.DataFrame,
+                         resample_week: int = 2,
+                         save_fig_name: str = None):
+    """
+    Calculate ACF / PACF / Seasonal test to prepare for regression
+
+    I remove ADFULLER test because it's very similar with ACF / PACF, and
+    it will focus on 1 period AR of overlapping periods.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Columns: at least ["prDec", "prInc", "ret"]
+        Index: (market: str, date: DatetimeIndex)
+    resample_week: int
+        Resample return series for evenly distributed interval
+    save_fig_name: str
+        If None, show() plot directly otherwise save with prefix defined here.
+    """
+    markets = df.index.get_level_values("market").unique()
+
+    figsize = (12, 4 * len(markets))
+    fig, ax = plt.subplots(len(markets), 2, figsize=figsize)
+    fig_s, ax_s = plt.subplots(len(markets), 1, figsize=figsize)
+    fig_f, ax_f = plt.subplots(len(markets), 1, figsize=figsize)
+
+    n = 0
+    dominant_freq = {}
+    season_dict = {}
+    for m in markets:
+        s = df.loc[m, "ret"].resample(f"{resample_week}W").first().ffill()
+
+        # ACF / PACF
+        sm.graphics.tsa.plot_acf(s, ax=ax[(n, 0)], title="ACF")
+        sm.graphics.tsa.plot_pacf(s, ax=ax[(n, 1)], title="PACF")
+        ax[(n, 0)].set_ylabel(m)
+
+        # Seasonal Decompose
+        season_results = seasonal_decompose(s)
+        season_dict[m] = pd.DataFrame({"trend": season_results.trend,
+                                       "seasonal": season_results.seasonal,
+                                       "resid": season_results.resid,
+                                       "observed": season_results.observed})
+        season_dict[m].plot(ax=ax_s[n], ylabel=m)
+
+        # Season FFT
+        fft_result = np.fft.fft(s)
+        power_spectrum = np.abs(fft_result) ** 2
+        freq = np.fft.fftfreq(len(s), d=resample_week)
+        dominant_freq[m] = freq[np.argsort(np.abs(fft_result))[::-1][1:]]
+        dominant_freq[m] = 1 / dominant_freq[m][dominant_freq[m] > 0][0]
+        ax_f[n].stem(1 / freq, power_spectrum)
+        ax_f[n].set_ylabel(m)
+        ax_f[n].set_title(f"max = {dominant_freq[m]}")
+        ax_f[n].set_xlim(0, s.shape[0])
+
+        n += 1
+
+    fig.suptitle("ACF vs PACF")
+    fig_s.suptitle("Seasonal Decompose")
+    fig_f.suptitle("FFT Power Spectrum (xlabel = weeks)")
+
+    if save_fig_name is None:
+        fig.show()
+        fig_s.show()
+        fig_f.show()
+    else:
+        fig.savefig(f"{save_fig_name}_acf.png")
+        fig_s.savefig(f"{save_fig_name}_seasonal.png")
+        fig_f.savefig(f"{save_fig_name}_fft.png")
+
+    return dominant_freq
+
+
+def compare_regressions(df: pd.DataFrame,
+                        model: Callable,
+                        model_ret_name: str = "endog",
+                        model_prob_name: str = "exog",
+                        resample_week: int = 2) -> dict[str, Any]:
+    """
+    Compare regression results of model with and without MPD data on returns
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Columns: at least ["prDec", "prInc", "ret"]
+        Index: (market: str, date: DatetimeIndex)
+    resample_week: int
+        Resample interval to convert return data
+    model: Callable
+        Partial function to initialize regression class,
+        e.g. SARIMAX, GARCHX, Markov Switching
+    model_ret_name: str
+        parameter name of the input return series
+    model_prob_name: str
+        parameter name of the input probability series
+    model: str
+        Partial function of model to use
+
+    Returns
+    -------
+    dictionary of {(market, self/prob): SARIMAResult class instance}
+    """
+    markets = df.index.get_level_values("market").unique()
+    results = {}
+    for m in markets:
+        df_resample = df.loc[m].resample(f"{resample_week}W").first().ffill()
+        y = df_resample["ret"]
+        X = df_resample[["prDec", "prInc"]]
+
+        results[(m, "without")] = model(**{model_ret_name: y}).fit()
+        results[(m, "with")] = model(**{model_ret_name: y,
+                                        model_prob_name: X}).fit()
+
+    return results
+
+
+def compare_regression_eval(results: dict[str, Any],
+                            metrics: str) -> pd.DataFrame:
+    """
+    Compare regression results by certain metrics
+
+    Parameters
+    ----------
+    results: dict[str, Any]
+        results of compare_regressions()
+    metrics: str
+        evaluation metrics to compare
+
+    Returns
+    -------
+    pd.DataFrame:
+        Columns: with (MPD data), without (MPD data), diff (= without - with)
+        Index: markets
+    """
+    df = pd.Series({
+        k: getattr(v, metrics) if hasattr(v, metrics) else np.nan
+        for k, v in results.items()
+    }).unstack()
+
+    df.loc[:, "diff"] = df["with"] - df["without"]
+    return df
+
--- test/test_explore_date.py
+++ test/test_explore_date.py
@@ -1,11 +1,63 @@
-from data_analysis.explore_data import calc_log_ret
-from data_analysis.download_data import get_price
+import pandas as pd
+from pytest import fixture
+
+from data_analysis.explore_data import (
+    calc_rolling_stat,
+    merge_prob_stat,
+    rename_stat_df,
+    plot_change_scatter,
+    prep_regression_stat,
+    compare_regressions,
+    compare_regression_eval
+)
+from data_analysis.download_data import get_price, get_mpd
 
 
-def test_calc_log_ret():
+@fixture
+def test_rolling_stat():
     df = get_price(tickers=["BAC", "C", "IYR", "SPY"],
                    start_date="2022-01-01", end_date="2023-12-31",
                    use_cache=True)
-    ret = calc_log_ret(df)
+    stat = calc_rolling_stat(df)
+    stat_rename = rename_stat_df(
+        stat, dict(zip(["bac", "citi", "iyr", "sp6m", "sp12m"],
+                       ["BAC", "C", "IYR", "SPY", "SPY"])))
 
-    assert False
+    return stat_rename
+
+
+@fixture
+def test_df(test_rolling_stat):
+    mpd = get_mpd()
+    df = merge_prob_stat(test_rolling_stat, mpd)
+
+    return df
+
+
+def test_plot_change_scatter(test_df):
+    plot_change_scatter(df=test_df, col="ret",
+                        hline_levels=[-0.2, 0.2])
+
+
+def test_prep_regression_stat(test_df):
+    prep_regression_stat(test_df)
+
+
+def test_compare_regressions(test_df):
+    from statsmodels.tsa.statespace.sarimax import SARIMAX
+    from arch import arch_model
+    import statsmodels.api as sm
+    from functools import partial
+
+    arima = compare_regressions(test_df, partial(SARIMAX, order=(1, 0, 1)))
+    garch = compare_regressions(
+        test_df, partial(arch_model, p=1, q=1, mean='ARX', vol='GARCH'),
+        model_ret_name="y", model_prob_name="x",
+    )
+
+    markov = compare_regressions(
+        test_df, partial(sm.tsa.MarkovRegression, k_regimes=3))
+
+    markov_vol = compare_regressions(
+        test_df, partial(sm.tsa.MarkovRegression, k_regimes=3,
+                         switching_variance=True))
